93c5f6238fb760b2bf5bddf2171778aad7bd15e9
Amarachukwu Ojiakor <amyojiakor@gmail.com>
2023-06-06 23:31:21 +0100

    Add solution for findMedianSortedArrays from LeetCode

---
+++ src/main/java/FindMedianSortedArrays.java
@@ -0,0 +1,65 @@
+public class FindMedianSortedArrays {
+
+    /**
+     * https://leetcode.com/problems/median-of-two-sorted-arrays/
+     *
+     * Given two sorted arrays nums1 and nums2 of size m and n respectively,
+     * return the median of the two sorted arrays.
+     *
+     * The overall run time complexity should be O(log (m+n)).
+     *
+     * Example 1:
+     *
+     * Input: nums1 = [1,3], nums2 = [2]
+     * Output: 2.00000
+     * Explanation: merged array = [1,2,3] and median is 2.
+     * Example 2:
+     *
+     * Input: nums1 = [1,2], nums2 = [3,4]
+     * Output: 2.50000
+     * Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+     *
+     * **/
+
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int[] merged = new int[m + n];
+
+        int i = 0, j = 0, k = 0;
+        while (i < m && j < n) {
+            if (nums1[i] <= nums2[j]) {
+                merged[k++] = nums1[i++];
+            } else {
+                merged[k++] = nums2[j++];
+            }
+        }
+
+        while (i < m) {
+            merged[k++] = nums1[i++];
+        }
+
+        while (j < n) {
+            merged[k++] = nums2[j++];
+        }
+
+        int length = merged.length;
+        int midIndex = length / 2;
+
+        if (length % 2 == 0) {
+            return (merged[midIndex - 1] + merged[midIndex]) / 2.0;
+        } else {
+            return merged[midIndex];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 3};
+        int[] nums2 = {2};
+        System.out.println(findMedianSortedArrays(nums1, nums2)); // Output: 2.00000
+
+        int[] nums3 = {1, 2};
+        int[] nums4 = {3, 4};
+        System.out.println(findMedianSortedArrays(nums3, nums4)); // Output: 2.50000
+    }
+}
